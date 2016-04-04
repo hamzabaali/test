536b78cabe2a92df807a0b14b791c7278b83e37d
hamzabaali <hamza2fes@gmail.com>
2016-04-04 17:06:59 +0200

    Added files via upload

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$age = 8;
+
+echo""    /* sans ; */
 echo "hello jenkins!!!!!!S";
+
+
+if ($age <= 12)
+{
+    echo "Salut gamin !";
+}
+else echo "Salut gros !";
 ?>
